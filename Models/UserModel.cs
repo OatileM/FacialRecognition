7cfd2034d894376eae7a4efb39487150a8ebc33e
Oatile Mdlela <Oatile.Mdlela@multichoice.co.za>
2024-06-23 16:10:48 +0200

    User model has been configured

---
+++ Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FacialRecognition.Models
+{
+    public class UserModel
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Phone { get; set; }
+
+
+        public UserModel(string id)
+        {
+            Id = id;
+        }
+    }
+}
